b3e111abcf95873a2473744cf363fac2519a2447
Stanley Ezeh <ezeh.stanley.es@gmail.com>
2024-01-31 16:26:11 +0100

    added challenge 1456

---
+++ leetcode75/1456_MaximumNumVowels.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file 1456_MaximumNumVowels.cpp
+ * @author Stanley Ezeh (ezeh.stanley.es@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-01-31
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        
+    }
+};
+
+
+int main()
+{
+    
+    return 0;
+}
